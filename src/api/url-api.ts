e8ee508682f58e859c18d083615f682c4cea4ea7
JhonSmith-1987 <77317038+JhonSmith-1987@users.noreply.github.com>
2024-11-24 15:10:34 -0500

    minor fix

--- src/api/url-api.ts
+++ src/api/url-api.ts
@@ -1,2 +1,2 @@
-// export const urlApi = 'https://chiropody-server-1.onrender.com';
-export const urlApi = 'http://localhost:4000';
+export const urlApi = 'https://chiropody-server-1.onrender.com';
+// export const urlApi = 'http://localhost:4000';
